ba117cb25aa1afa1b58716db5c14df7f24b39d6e
Hengyu Jiang <hengyu.jiang@my.jcu.edu.au>
2026-05-13 18:58:06 +0800

    fix: 修复mock接口，完成table数据请求

--- src/api/mock.js
+++ src/api/mock.js
@@ -1,7 +1,7 @@
 import Mock from 'mockjs'
 import homeData from './mockData/homeData'
 
-
-Mock.mock('/\/api\/home\/getTableData/', 'get', 
+//查错笔记：正则表达式不要忘记删掉两边的引号，否则会被当成字符串而不是正则表达式
+Mock.mock(/\/api\/home\/getTableData/, 'get', 
     homeData.getTableData
 )
